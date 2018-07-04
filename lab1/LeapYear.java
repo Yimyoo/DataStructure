bc9ece4d647e3bf84bae46db70f3ac45d1537c5e
Your Name <729026597@qq.com>
2018-07-03 17:12:50 -0700

    Completed part Leap Year

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,6 +1,8 @@
 /** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
  */
+import java.lang.Boolean;
+
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
@@ -14,6 +16,11 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year) {
+	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) return true;
+	else return false;
+    }
+    
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
